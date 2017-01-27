619f33392ce0945b3d9fa3fa4d466048d66d575a
Matthias Van Ceulebroeck <matthias.vanceulebroeck@student.kuleuven.be>
2017-01-27 20:52:59 +0100

    small fix

--- js/actions.js
+++ js/actions.js
@@ -1,22 +1,26 @@
-$(document).ready(loadMap());
-
-$("#fieldTable td").hover(function() {
-	 $(this).css('background-color', '#fsf8aa');
-}, function () {
-    $(this).css('background-color', '#ffffff');
-});
+/*$(document).load(loadMap());
 
 function loadMap()
 {
-	var table = document.getElementById("fieldTable");
+	$(".navigation div a").html("lel");
+	var table = $("#fieldTable");
 
 	for (var i = 0; i < table.rows.length; i++) {
 		for (var j = 0; j < table.rows[0].cells.length; j++) {
 			var cellContent = table.rows[i].cells[j].innerHTML;
-			if (cellContent === "")
+			if (cellContent === "") {
 				cellContent = "<img src='images/totem.png' alt='TOTEM'/>";
-			else if (cellContent === "CHEST")
+			}
+			else if (cellContent === "CHEST") {
 				cellContent = "<img src='images/chest.png' alt='CHEST'/>";
+			}
 		}
 	}
-}
+}*/
+
+$('#fieldTable td').hover(function() {
+	$(this).css('background-color', '#E521D8')
+}, function() {
+  $(this).css('background-color', '#000000')
+});
+
