5fa86c9bbe720f4454d55c41d59345a8345ebc38
Matthias Van Ceulebroeck <matthias.vanceulebroeck@student.kuleuven.be>
2017-01-27 19:36:49 +0100

    correct fix

--- js/actions.js
+++ js/actions.js
@@ -10,14 +10,13 @@ function loadMap()
 {
 	var table = document.getElementById("fieldTable");
 
-	for (i = 0; i < table.rows.length; i++) {
-		for (j = 0; j < table.rows[0].cells.length; j++) {
+	for (var i = 0; i < table.rows.length; i++) {
+		for (var j = 0; j < table.rows[0].cells.length; j++) {
 			var cellContent = table.rows[i].cells[j].innerHTML;
-			if (cellContent == "")
+			if (cellContent === "")
 				cellContent = "<img src='images/totem.png' alt='TOTEM'/>";
-			else if (cellContent == "CHEST")
-				cellContent = "<svg src='images/chestBottom.svg' alt='CHEST'/>
-			<svg src='images/chestTop.svg' alt='CHEST'/>"
+			else if (cellContent === "CHEST")
+				cellContent = "<img src='images/chest.png' alt='CHEST'/>";
 		}
 	}
 }
